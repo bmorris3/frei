1fbbfc25abc47bd56dc8af921bd1b179a6d88991
Brett Morris <brettmorris21@gmail.com>
2022-02-09 09:55:41 +0100

    excluding notebook checkpoints from docs build

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,10 @@ extensions = [
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+    '_build', 'Thumbs.db', '.DS_Store', 
+    '.ipynb_checkpoints', '*/.ipynb_checkpoints', '*/*/.ipynb_checkpoints'
+]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
